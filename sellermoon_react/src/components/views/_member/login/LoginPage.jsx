5d5214c70d8583bcec4fc67aa5436a99049ffaf0
rlagmlfk <gmlfk703@gmail.com>
2022-10-24 23:44:01 +0900

    221024 로그인 로직 변경

--- sellermoon_react/src/components/views/_member/login/LoginPage.jsx
+++ sellermoon_react/src/components/views/_member/login/LoginPage.jsx
@@ -25,11 +25,22 @@ const LoginPage = (props) => {
       .then((res) => {
         console.log(res);
         console.log(res.data);
-        if (res.data.member_email === email) {
+        // 암호화된 비밀번호와 입력한 비밀번호가 스프링 단에서 비교되기 때문에
+        // 출력되는 비밀번호와 res.data.member_password는 서버에서 비교하기엔 다른 값
+        // 만약 틀린 비밀번호를 입력한다면 res.data.member_password는 입력한 password와
+        // 같은 값으로 출력되므로 res.data.member_password !== password 조건을 추가해 로그인 실패를 비교
+        if (
+          res.data.member_email === email &&
+          res.data.member_password !== password
+        ) {
           console.log("로그인 성공");
           sessionStorage.setItem("user_id", email);
           sessionStorage.setItem("user_name", res.data.member_name);
           navigate("/monthlymoon/register");
+          // 로그인을 실패하면 스프링에서 입력한 값만 vo에 담아 보내기때문에
+          // member_no의 값은 0이 출력됨 -> 로그인을 실패한 걸 알 수 있음
+        } else if (res.data.member_no === 0) {
+          alert("이메일 또는 비밀번호를 확인하세요");
         }
       })
       .catch();
